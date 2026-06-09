9f88ed2baf89a724c4b6ea536e0d38ee31ff6ad8
jvdsouza <jvincentdsouza@gmail.com>
2026-06-09 18:25:11 +1000

    fix env variable reading and linking

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jvdsouza.github.io',
-  base: '/portfolio',
+  base: '/portfolio/',
+  vite: {
+    envPrefix: ['PUBLIC_', 'VITE_', 'PORTFOLIO_'],
+  },
 });
